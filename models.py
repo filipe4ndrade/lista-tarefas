88ebbdc56fa58ffa045078fc39ff3a1346732847
Jose Filipe <filipeandrade000@gmail.com>
2024-11-19 02:40:15 -0300

    cursor focado em Nome/ Custo >=0 / Formatação de exibição ok/ Limitação pra custo

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Numeric
+
 db = SQLAlchemy()
 
 class Tarefa(db.Model):
     __tablename__ = 'tarefas'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), unique=True, nullable=False)
-    custo = db.Column(db.Float, nullable=False)
+    custo = db.Column(Numeric(precision=50, scale=2), nullable=False)
     data_limite = db.Column(db.Date, nullable=False)
     ordem = db.Column(db.Integer, unique=True, nullable=False)
